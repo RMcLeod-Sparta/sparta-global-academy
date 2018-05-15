705845c1369c2e60b7eafef918ceb17265fa9de2
Tech-A06 <tech-a06@tech-a06s-air.home>
2018-05-15 21:26:12 +0100

    working CRUD for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'user/name'
-  get 'user/email'
-  get 'user/address'
+
   resources :computers
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

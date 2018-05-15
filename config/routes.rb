19810d6f030edc0a535d3207fc04c4b47085d50d
Tech-A06 <tech-a06@tech-a06s-air.home>
2018-05-15 21:00:15 +0100

    both resources are in place

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'user/name'
+  get 'user/email'
+  get 'user/address'
   resources :computers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_194008) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "computers", force: :cascade do |t|
-    t.string "name"
-    t.string "availability"
-    t.string "color"
-    t.string "serial_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
